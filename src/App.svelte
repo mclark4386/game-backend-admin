e844a7e494c998e0e9931ce2b9583767de30c70f
Matthew Clark <484943+mclark4386@users.noreply.github.com>
2021-02-11 04:02:49 -0600

    *try that for debugging

--- src/App.svelte
+++ src/App.svelte
@@ -14,103 +14,112 @@
   });
 
   let version = 0;
-  $: labeledVersion = "Version:"+version;
+  $: labeledVersion = "Version:" + version;
 
-  $: if(version >= 10){
+  $: if (version >= 10) {
     alert("Welcome to a new world!");
   }
 
-  function handleClick(){
+  function handleClick() {
     version += 1;
   }
   async function refreshDate() {
     const res = await fetch("/api/date");
     const text = await res.text();
 
-		if (res.ok) {
-			return text;
-		} else {
-			throw new Error(text);
-		}
+    if (res.ok) {
+      return text;
+    } else {
+      throw new Error(text);
+    }
   }
   async function refreshError() {
     const res = await fetch("/api/error");
     const text = await res.text();
 
-		if (res.ok) {
-			return text;
-		} else {
-			throw new Error(text);
-		}
+    if (res.ok) {
+      return text;
+    } else {
+      throw new Error("(" + res.code + "): " + text);
+    }
   }
-  function refresh(){
+  function refresh() {
     datePromise = refreshDate();
     errorPromise = refreshError();
   }
 </script>
 
 {#if !loggedIn}
-  <input placeholder="Email"/> <br/>
-  <input placeholder="Password" type="password"/> <br/>
-  <button on:click={()=>loggedIn = !loggedIn}>Login</button>
-{:else}
-<main>
-  <h1>Svelte + Node.js API</h1>
-  <button on:click={()=>loggedIn = !loggedIn}>Logout</button>
-  <h2>
-    Deployed with
-    <a href="https://vercel.com/docs" target="_blank" rel="noreferrer noopener">
-      Vercel
-    </a>
-    !
-  </h2>
-  <p>
-    <a
-      href="https://github.com/vercel/vercel/tree/master/examples/svelte"
-      target="_blank"
-      rel="noreferrer noopener">
-      This project
-    </a>
-    is a
-    <a href="https://svelte.dev/">Svelte</a>
-    app with three directories,
-    <code>/public</code>
-    for static assets,
-    <code>/src</code>
-    for components and content, and
-    <code>/api</code>
-    which contains a serverless
-    <a href="https://nodejs.org/en/">Node.js</a>
-    function. See
-    <a href="/api/date">
-      <code>api/date</code>
-      for the Date API with Node.js
-    </a>
-    .
-  </p>
+  <input placeholder="Email" />
   <br />
-  <h2 on:click={refresh}>The date according to Node.js is:</h2>
-  {#await datePromise}
-    <p>...loading date</p>
-  {:then date}
-    <p>{date ? date : 'Loading date...'}</p>
-  {:catch error}
-    <p style="color: red">{error.message}</p>
-  {/await}
-<br/>
-Error:
-  {#await errorPromise}
-    <p>...loading error</p>
-  {:then date}
-    <p>{date ? date : 'Loading error...'}</p>
-  {:catch error}
-    {console.log(error)}
-    <p style="color: red">({error}){error.message}</p>
-  {/await}
+  <input placeholder="Password" type="password" />
+  <br />
+  <button on:click={() => (loggedIn = !loggedIn)}>Login</button>
+{:else}
+  <main>
+    <h1>Svelte + Node.js API</h1>
+    <button on:click={() => (loggedIn = !loggedIn)}>Logout</button>
+    <h2>
+      Deployed with
+      <a
+        href="https://vercel.com/docs"
+        target="_blank"
+        rel="noreferrer noopener">
+        Vercel
+      </a>
+      !
+    </h2>
+    <p>
+      <a
+        href="https://github.com/vercel/vercel/tree/master/examples/svelte"
+        target="_blank"
+        rel="noreferrer noopener">
+        This project
+      </a>
+      is a
+      <a href="https://svelte.dev/">Svelte</a>
+      app with three directories,
+      <code>/public</code>
+      for static assets,
+      <code>/src</code>
+      for components and content, and
+      <code>/api</code>
+      which contains a serverless
+      <a href="https://nodejs.org/en/">Node.js</a>
+      function. See
+      <a href="/api/date">
+        <code>api/date</code>
+        for the Date API with Node.js
+      </a>
+      .
+    </p>
+    <br />
+    <h2 on:click={refresh}>The date according to Node.js is:</h2>
+    {#await datePromise}
+      <p>...loading date</p>
+    {:then date}
+      <p>{date ? date : 'Loading date...'}</p>
+    {:catch error}
+      <p style="color: red">{error.message}</p>
+    {/await}
+    <br />
+    Error:
+    {#await errorPromise}
+      <p>...loading error</p>
+    {:then date}
+      <p>{date ? date : 'Loading error...'}</p>
+    {:catch error}
+      {console.log(error)}
+      <p style="color: red">({error}){error.message}</p>
+    {/await}
 
-  <p><b><button on:click={handleClick}>{labeledVersion}</button></b>  <i>int({version})</i></p>
-  <p>Total Quizzes: {quizzes.length}</p>
-  <QuizList {quizzes}/>
-</main>
-
+    <p>
+      <b>
+        <button on:click={handleClick}>{labeledVersion}</button>
+      </b>
+      <i>int({version})</i>
+    </p>
+    <p>Total Quizzes: {quizzes.length}</p>
+    <QuizList {quizzes} />
+  </main>
 {/if}
